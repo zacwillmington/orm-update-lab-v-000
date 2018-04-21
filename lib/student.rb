30c21f89d540459e87138b26894dbeb550bd0327
Zac Willmington <zac.willmington@gmail.com>
2018-04-21 21:17:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,7 @@ class Student
       WHERE name = ?;
       SQL
       student_found = DB[:conn].execute(sql, name).first
+      binding.pry
       self.create(student_found[1], student_found[2])
   end
 
