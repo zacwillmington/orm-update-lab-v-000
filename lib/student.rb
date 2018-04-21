d02ae6a09c9057a8b5a4f394805578af2dc51c92
Zac Willmington <zac.willmington@gmail.com>
2018-04-21 21:08:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
         CREATE TABLE IF NOT EXISTS students(
          id INTEGER PRIMARY KEY,
          name TEXT,
-         grade INTEGER
+         grade TEXT
         )
       SQL
       DB[:conn].execute(sql)
