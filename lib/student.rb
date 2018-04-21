f2c2cddc62e130c1c6b909d9aa43d87cd07170e5
Zac Willmington <zac.willmington@gmail.com>
2018-04-21 21:27:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,8 +51,7 @@ class Student
   end
 
   def self.new_from_db(row)
-      new_student = self.new(row[1], row[2])
-      new_student.id = row[0]
+      new_student = self.new(row[0], row[1], row[2])
 
   end
 
